1b506ea7e1f7f5771b285f7eeef5bb3f562347a1
Jeremy <hurleyjer@yahoo.com>
2018-05-06 21:31:40 -0400

    add port info to server

--- server.js
+++ server.js
@@ -49,6 +49,13 @@ app.get('*', (req, res) => {
 /*
  * START NODE SERVER 
 */
-app.listen(3000, function () {
-    console.log('API running on localhost:3000');
-})
+// app.listen(3000, function () {
+//     console.log('API running on localhost:3000');
+// })
+
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`API running on localhost:${port}`));
